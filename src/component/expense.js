fb2e8efc5da2bf1336e77271c3b5cc0bfb99f6ba
igennova <luckynegi1025@gmail.com>
2023-04-17 00:45:17 +0530

    ready

--- src/component/expense.js
+++ src/component/expense.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
 import "./expense.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+  const exdate=new Date(2023,3,10);
+  const extitle="lucky";
+  const amount=1000;
+
   return (
     <div className="expense-item">
-      <div>Date</div>
+      <div>{props.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Title</h2>
+        <h2>{props.title}</h2>
       </div>
-      <div className="expense-item__price">Amount</div>
+      <div className="expense-item__price">{props.amount}</div>
     </div>
   );
 }
